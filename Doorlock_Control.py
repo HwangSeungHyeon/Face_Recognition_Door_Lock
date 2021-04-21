144997a73c4f9f8fe09ce3a892626b73c45fa7ae
HwangSeungHyeon <xlddy02@gmail.com>
2021-04-22 00:13:47 +0900

    delete and update

--- Doorlock_Control.py
+++ Doorlock_Control.py
@@ -1,15 +1,16 @@
 # 이 코드는 raspberry pi에서 사용하는 코드입니다.
-# only raspberri pi
+# unlock() 코드를 실행하면 지정한 핀에 전류가 흐릅니다.
+# lock() 코드를 실행하면 지정한 핀의 전류가 끊깁니다.
 
 import RPi.GPIO as GPIO
 import time
 
-def unlock(pin=18):
+def unlock(pin = 18):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin,GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
     
-def lock(pin=18):
+def lock(pin = 18):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin,GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
